fc5288a4eb6962e2cd8942b69861b221bd6593ce
andreyka101 <andrejkvasnin236@gmail.com>
2023-12-16 20:04:04 +0300

    уауауа

--- class.py
+++ class.py
@@ -16,6 +16,8 @@ class animals():
         print(self.name+" ran")
     def showYears(self):
         return self.__private_years
+    def retALL (self):
+        return [self.name, self.__private_years]
     
 
 
@@ -39,3 +41,48 @@ print(cat.showYears())
 # для работы с этим объектом.
 # 1. Функция для вывода на экран информации об автомобиле.
 # 2. Функция для подсчета необходимого времени для преодоления переданного расстояния со средней скоростью.
+
+
+
+# урок 2 
+
+class transport():
+    ani = animals()
+
+    def __init__(self, name : str, speed : int , numberPassengers : int , machineWeight : int):
+        """.metod(name = str , speed = num , numberPassengers = num , machineWeight = num)"""
+        self.name = name
+        self.speed = speed
+        self.numberPassengers = numberPassengers
+        self.machineWeight = machineWeight
+
+
+    def timeTravel(self, distance):
+        """aniani"""
+        return distance/(self.speed-self.machineWeight) , "час"
+    def wer (self):
+        print(self.speed)
+    
+
+class car(transport):
+    def __init__(self, name: str, speed: int, numberPassengers: int, machineWeight: int , petrol : int):
+        super().__init__(name, speed, numberPassengers, machineWeight)
+        # остановился здесь
+        self.petrol = petrol
+    
+
+
+bike = transport("my bike", 20 , 1 , 3)
+print(bike.timeTravel(100))
+bike.ani.ran()
+
+carRed = car("my car" , 300 , 2 , 50 , 60)
+print(carRed.timeTravel(100))
+
+
+# домашнее задание урока 2
+# номер 1
+# создать класс колькулятор 
+# в конструкторе нужно в писать 2 числа
+# создать 4 метода: умножение , деление , сумма , вычитание
+# создать метод для добавления числа (его можно вызвать много раз и подучить много чисел)
