358026cda51f6f071ae636d14aece463a3d4e2da
andreyka101 <andrejkvasnin236@gmail.com>
2024-01-13 23:47:46 +0300

    feefef

--- data.py
+++ data.py
@@ -32,9 +32,49 @@ dataWrite.close()
 
 
 # урок 2 ----------------
+# file = open("B:/del.txt",'w')
+# file.write("00000")
+# file.close()
+# rd = open("B:/del.txt","r").read()
+# print(rd)
 
-# read = open("C:/Games/sel.txt",'r').read()
-# print(read)
+
+# урок 3 ----------------
+
+# with - закроет файл автоматически при завершении блока
+# with open("B:/del.txt",'w') as file:
+#     file.write(input())
+
+# rd = open("B:/del.txt","r").read()
+# print(rd)
 
 
+# with - также его можно использовать для чтения файла
+# with open("B:/del.txt",'r') as file:
+#     fileRead = file.read()
+# метод .rstrip() убирает в конце строки пробелы
+#     print(fileRead.rstrip())
 
+
+with open("B:/del.txt",'r') as file:
+    # метод .readlines() каждую строку txt файла превращает в элемент массива
+    # он возвращает массив
+    fileRead = file.readlines()
+    print(fileRead)
+    checkForIndentation = True
+    fileARR = []
+    for i in fileRead:
+        if (i == '  ' or i == "\n"):
+            checkForIndentation = False
+        if(checkForIndentation):
+            fileARR.append(i)
+    print(fileARR)
+
+
+# домашнее задание урока 3
+# номер 1
+# создайте менеджер пользователей
+# . программа должна работать до тех пор пока не не будет введено exit или ex
+# . если ввести new - создаётся новый пользователь он должен хранить имя и пароль (если ввести существующие имя то программа просит его изменить)
+# . если ввести del - удаляется пользователь его имя и пароль
+# все пользователи должны сохраняются на txt файл
