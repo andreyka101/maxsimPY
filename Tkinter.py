34df71b67d20776c6643c598c4b976dfe7d7f051
andreyka101 <andrejkvasnin236@gmail.com>
2024-02-03 16:26:09 +0300

    efefe

---
+++ Tkinter.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+window = Tk()
+window.title("12345")
+window.geometry('400x250')
+
+text = Label(text="Привет")
+text.grid(column=0, row=0)
+
+
+
+
+window.mainloop()
---
+++ dictionary.py
@@ -0,0 +1,35 @@
+obj = {
+    "key": "qwerty",
+    "key2": 4555,
+    "key3": [1,2,3,4],
+    "444": 454,
+}
+# obj.clear()
+
+
+# copy = obj.copy()
+# obj.clear()
+
+
+# for k, v in obj.items():
+#     print(k, "=", v)
+
+# def f (x):
+#     return x+"_"
+# h = list(map(f, obj.items()))
+
+
+# print(obj.keys())
+# print(obj.values())
+
+
+# obj.pop('key2')
+
+
+# obj.popitem()
+
+
+# obj.update({"ttt":9,"poi":"uuu"})
+
+
+print(obj)
