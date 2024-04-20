19de4d6a6921a049a141072fb22fc993edee577c
andreyka101 <andrejkvasnin236@gmail.com>
2024-04-20 18:53:42 +0300

    efefef

--- robot.py
+++ robot.py
@@ -1,13 +1,24 @@
 from tkinter import *
+import json
+import random
+
 window = Tk()
-moveY = 0
-moveX = 0
+
+dataRead = open("file_robot.json","r").read()
+file = json.loads(dataRead)
+moveY = file['y']
+moveX = file['x']
+
+boxPositionY = 50 * random.randint(0, 11)
+boxPositionX = 50 * random.randint(0, 9)
 
 
 def keyPress(event):
     global moveY
     global moveX
-    text.config(text=event)
+    global boxPositionY
+    global boxPositionX
+    # text.config(text=event)
     if (event.keysym == "Down"):
         if(moveY < 450):
             moveY += 50
@@ -25,10 +36,21 @@ def keyPress(event):
             moveX += 50
         robot.config(bg = "#3dffc5" )
     robot.place(y = moveY , x = moveX)
+    if (boxPositionY == moveY and boxPositionX == moveX):
+        boxPositionY = 50 * random.randint(0, 10)
+        boxPositionX = 50 * random.randint(0, 8)
+        box.place(x=boxPositionX, y=boxPositionY)
+        text.config(text=str(boxPositionX) + " " + str(boxPositionY))
+
+    with open("file_robot.json", "w") as file_X_Y:
+        file_X_Y.write(json.dumps({
+            "x": moveX,
+            "y": moveY,
+        }))
 
 
 def keyRelease(event):
-    text.config(text=event)
+    # text.config(text=event)
     robot.config(bg = "red")
 
 
@@ -39,7 +61,10 @@ window.bind("<KeyPress>" , keyPress)
 window.bind("<KeyRelease>" , keyRelease)
 
 robot = Label(bg = "red")
-robot.place(h=50 , w=50 , x=0 , y = 0)
+robot.place(h=50 , w=50 , x=moveX , y = moveY)
+
+box = Label(bg = "#d5d900")
+box.place(h=50 , w=50 , x=boxPositionX , y = boxPositionY)
 
 text = Label(text="ответ:", font='Times 15')
 text.place(h=20 , x=170 , y = 85)
---
+++ tk_image.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from PIL import Image, ImageTk
+root = Tk()
+
+# pip install Pillow
+
+# image = Image.open("image.jpg")
+# image = Image.open("image_2.jpg")
+image = Image.open("happy.gif")
+scaled_image = image.resize((500, 300))
+photo = [ImageTk.PhotoImage(file='mygif.gif',format = 'gif -index %i' %(i)) for i in range(100)]
+# photo = ImageTk.PhotoImage()
+
+labImage = Label(image=photo)
+labImage.place(x=0, y=0)
+# labImage.pack()
+
+root.mainloop()
