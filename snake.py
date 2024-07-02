eda877e09f765b4bfd7be4383b2208fd26c2310e
andreyka101 <andrejkvasnin236@gmail.com>
2024-07-02 17:27:21 +0300

    dv

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 import time 
 from tkinter import *
+import random
 start = time.time()
 timePassed = time.time() - start
 speed = 50
@@ -11,10 +12,17 @@ move_snake_number_XY = {
     "x2":100,
     "y2":50,
 }
-move_snake_number_x = 0
-move_snake_number_y = 0
+arr_move_snake_number = [
+     {
+     "move_snake_number_x" : 0,
+     "move_snake_number_y" : 0,
+},
+]
 move_snake_number_wh = 50
 timePassed_old = 0
+
+applePositionY = 50 * random.randint(0, 9)
+applePositionX = 50 * random.randint(0, 11)
 
 
 
@@ -37,7 +45,9 @@ canV = Canvas(width=600, height=500, bg='white')
 text_l = Label(text="ответ:")
 canV.pack()
  
-canV.create_rectangle(move_snake_number_x, move_snake_number_y, move_snake_number_x + move_snake_number_wh, move_snake_number_y + move_snake_number_wh,fill='green', outline='green')
+canV.create_rectangle(arr_move_snake_number[0]["move_snake_number_x"], arr_move_snake_number[0]["move_snake_number_y"], arr_move_snake_number[0]["move_snake_number_x"] + move_snake_number_wh, arr_move_snake_number[0]["move_snake_number_y"] + move_snake_number_wh,fill='green', outline='green')
+
+canV.create_rectangle(applePositionX, applePositionY, applePositionX + move_snake_number_wh, applePositionY + move_snake_number_wh,fill='green', outline='green')
 
 while(timePassed <= 2000):
     timePassed_old = timePassed
@@ -49,38 +59,77 @@ while(timePassed <= 2000):
     print(move_snake)
     if(move_snake == "Right" and round(timePassed_old) != round(timePassed)):
             if(move_snake_old == "Left"):
-                  move_snake_number_x -= speed
+                arr_move_snake_number[0]["move_snake_number_x"] -= speed
+                if(arr_move_snake_number[0]["move_snake_number_x"]<0):
+                    arr_move_snake_number[0]["move_snake_number_x"] = 550
             else: 
-                move_snake_number_x += speed
+                arr_move_snake_number[0]["move_snake_number_x"] += speed
+                if(arr_move_snake_number[0]["move_snake_number_x"]>550):
+                    arr_move_snake_number[0]["move_snake_number_x"] = 0
                 move_snake_old = move_snake
     if(move_snake == "Left" and round(timePassed_old) != round(timePassed)):
             if(move_snake_old == "Right"):
-                  move_snake_number_x += speed
+                arr_move_snake_number[0]["move_snake_number_x"] += speed
+                if(arr_move_snake_number[0]["move_snake_number_x"]>550):
+                    arr_move_snake_number[0]["move_snake_number_x"] = 0
             else: 
-                move_snake_number_x -= speed
+                arr_move_snake_number[0]["move_snake_number_x"] -= speed
+                if(arr_move_snake_number[0]["move_snake_number_x"]<0):
+                    arr_move_snake_number[0]["move_snake_number_x"] = 550
                 move_snake_old = move_snake
     elif(move_snake == "Down" and round(timePassed_old) != round(timePassed)):
             if(move_snake_old == "Up"):
-                move_snake_number_y -= speed
-                if(move_snake_number_y<0):
-                    move_snake_number_y =450
+                arr_move_snake_number[0]["move_snake_number_y"] -= speed
+                if(arr_move_snake_number[0]["move_snake_number_y"]<0):
+                    arr_move_snake_number[0]["move_snake_number_y"] =450
             else: 
-                if(move_snake_number_y>450):
-                    move_snake_number_y =0
-                move_snake_number_y += speed
+                arr_move_snake_number[0]["move_snake_number_y"] += speed
+                if(arr_move_snake_number[0]["move_snake_number_y"]>450):
+                    arr_move_snake_number[0]["move_snake_number_y"] = 0
                 move_snake_old = move_snake
     elif(move_snake == "Up" and round(timePassed_old) != round(timePassed)):
             if(move_snake_old == "Down"):
-                move_snake_number_y += speed
-                if(move_snake_number_y>550):
-                    move_snake_number_y =0
-            else: 
-                move_snake_number_y -= speed
-                if(move_snake_number_y<0):
-                    move_snake_number_y =450
+                arr_move_snake_number[0]["move_snake_number_y"] += speed
+                if(arr_move_snake_number[0]["move_snake_number_y"]>450):
+                    arr_move_snake_number[0]["move_snake_number_y"] = 0
+            else:
+                for i in range(len(arr_move_snake_number)):
+                     
+                arr_move_snake_number[0]["move_snake_number_y"] -= speed
+                if(arr_move_snake_number[0]["move_snake_number_y"]<0):
+                    arr_move_snake_number[0]["move_snake_number_y"] = 450
                 move_snake_old = move_snake
+    
+    if(arr_move_snake_number[0]["move_snake_number_y"] == applePositionY and arr_move_snake_number[0]["move_snake_number_x"] == applePositionX):
+        # canV.create_rectangle(arr_move_snake_number[0]["move_snake_number_x"], arr_move_snake_number[0]["move_snake_number_y"], arr_move_snake_number[0]["move_snake_number_x"] + move_snake_number_wh, arr_move_snake_number[0]["move_snake_number_y"] + move_snake_number_wh,fill='green', outline='green')
+        if(move_snake == "Right"):
+             arr_move_snake_number.append({
+                "move_snake_number_x" : arr_move_snake_number[0]["move_snake_number_x"] - speed,
+                "move_snake_number_y" : arr_move_snake_number[0]["move_snake_number_y"],
+            })
+        elif(move_snake == "Left"):
+             arr_move_snake_number.append({
+                "move_snake_number_x" : arr_move_snake_number[0]["move_snake_number_x"] + speed,
+                "move_snake_number_y" : arr_move_snake_number[0]["move_snake_number_y"],
+            })
+        elif(move_snake == "Down"):
+             arr_move_snake_number.append({
+                "move_snake_number_x" : arr_move_snake_number[0]["move_snake_number_x"],
+                "move_snake_number_y" : arr_move_snake_number[0]["move_snake_number_y"] - speed,
+            })
+        elif(move_snake == "Up"):
+             arr_move_snake_number.append({
+                "move_snake_number_x" : arr_move_snake_number[0]["move_snake_number_x"],
+                "move_snake_number_y" : arr_move_snake_number[0]["move_snake_number_y"] + speed,
+            })
+        for i in arr_move_snake_number:
+            while(i["move_snake_number_y"] == applePositionY and i["move_snake_number_x"] == applePositionX):
+                applePositionY = 50 * random.randint(0, 9)
+                applePositionX = 50 * random.randint(0, 11)
     canV.create_rectangle(0, 0, 600, 500,fill='white', outline='white')
-    canV.create_rectangle(move_snake_number_x, move_snake_number_y, move_snake_number_x + move_snake_number_wh, move_snake_number_y + move_snake_number_wh,fill='green', outline='green')
+    canV.create_arc(applePositionX, applePositionY, applePositionX + move_snake_number_wh, applePositionY + move_snake_number_wh,fill='red', outline='red',start=0,extent=359)
+    for i in arr_move_snake_number:
+        canV.create_rectangle(i["move_snake_number_x"], i["move_snake_number_y"], i["move_snake_number_x"] + move_snake_number_wh, i["move_snake_number_y"] + move_snake_number_wh,fill='green', outline='green')
     # canV.create_rectangle(0  + round(timePassed) * speed, 0, 50  + round(timePassed) * speed, 50,fill='green', outline='green')
 
         
